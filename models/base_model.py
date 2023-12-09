75e1b9b16842ec68b24b6f94a936135dee115e97
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-09 11:24:28 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,53 @@
 #!/usr/bin/python3
-
-import uuid
+''' module for BaseModel class '''
 from datetime import datetime
+import uuid
+import models
+
 
 class BaseModel:
+    ''' BaseModel class '''
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-            self.id = kwargs.get('id', str(uuid.uuid4()))
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        '''
+        initation of basemodel
+
+        Args:
+        *args: arguments passed in
+        **kwargs: arguments with key values
+
+        Return:
+        None
+        '''
+        if len(kwargs) != 0:
+            self.__dict__ = kwargs
+            self.created_at = datetime.strptime(self.created_at,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(self.updated_at,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            from models import storage  # Import inside the method to avoid circular import
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        '''
+        Return:
+        string represntation fo object
+        '''
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
+        ''' updates date for updated_at attribute '''
         self.updated_at = datetime.now()
-        from models import storage  # Import inside the method to avoid circular import
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        result_dict = self.__dict__.copy()
-        result_dict['__class__'] = self.__class__.__name__
-        result_dict['created_at'] = self.created_at.isoformat()
-        result_dict['updated_at'] = self.updated_at.isoformat()
-        return result_dict
+        ''' returns dictonary with all key values of instance '''
+        copy_dict = self.__dict__.copy()
+        copy_dict['__class__'] = self.__class__.__name__
+        copy_dict['created_at'] = self.created_at.isoformat()
+        copy_dict['updated_at'] = self.updated_at.isoformat()
+
+        return copy_dict
