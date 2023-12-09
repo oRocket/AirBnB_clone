f2ebe38c7e48bbc2e044119ef29a73edd6fd854e
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-09 07:51:17 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,22 @@
-#!/usr/bin/python3
-
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
