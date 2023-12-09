beb6379c61f490cdd2d3d92424f31668492dff68
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-09 05:29:29 -0800

    fixing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,57 @@
-#!/bin/python3
-
+#!/usr/bin/python3
+''' FileStorage module '''
 import json
+from models.base_model import BaseModel
 import models
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-class FileStorage():
-  __file_path = "file.json"
-  __objects = {}
-  
-  def all(self):
-    return self.__objects
-    
-  def new(self, obj):
-    key = f"{obj.__class__.__name__}.{obj.id}"
-    self.__objects[key] = obj
-  
-  def save(self):
-    with open(self.__file_path, "w", encoding="utf-8") as f:
-        d = {k : v.to_dict() for k, v in self.__objects.items()}
-        json.dump(d, f)
-      
-  def reload(self):
-    try:
-      with open(self.__file_path, "r", encoding="utf-8") as f:
-              obj_dict = json.load(f)
-              for o in obj_dict.values():
-                clas_name = o["__class__"]
-                del o["__class__"]
-                self.new(eval(f"models.{clas_name}")(**o))
-                
-    except FileNotFoundError:
-      return
+
+class FileStorage:
+    '''FileStorage class'''
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """
+        Return: the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets in objects with key classname.id
+
+        Args:
+        object
+        """
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        '''
+        serializes __objects to JSON file
+        '''
+        new_dict = {}
+        with open(self.__file_path, mode='w+', encoding='utf-8') as f:
+            for k, v in self.__objects.items():
+                new_dict[k] = v.to_dict()
+            json.dump(newdict, f)
+
+    def reload(self):
+        '''
+        deserializes the JSON file
+        '''
+        try:
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                newobj = json.load(f)
+                for k, v in newobj.items():
+                    reloadedobj = eval('{}(**v)'.format(v['__class__']))
+                    self.__objects[k] = reloadedobj
+
+        except IOError:
+            pass
